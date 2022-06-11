bbd1548fbc3e1b3707ba631e5b12054f78068da5
Mogleson Lima <moglesonlima@alu.ufc.br>
2022-06-11 01:15:32 -0300

    Desafio 03

--- main.tf
+++ main.tf
@@ -7,38 +7,46 @@ terraform {
   }
 }
 
-# Configure the DigitalOcean Provider
 provider "digitalocean" {
-  token = var.do_token
-  #"dop_v1_9a3ebfbfcaed563466b9b812f5609edb85b0958626a02a02ad3be37c04310802"
+    token = var.do_token
+    
 }
 
-resource "digitalocean_kubernetes_cluster" "k8s_iniciativa_DevOps" {
+resource "digitalocean_kubernetes_cluster" "k8s_iniciativa" {
   name   = var.k8s_name
-  #"k8s-iniciativa-devops"
   region = var.region
-  #"nyc1"
   # Grab the latest version slug from `doctl kubernetes options versions`
   version = "1.22.8-do.1"
 
   node_pool {
-    name       = "default"
+    name       = "worker-pool"
     size       = "s-2vcpu-2gb"
     node_count = 2
   }
 }
 
-#Node pool
+
 resource "digitalocean_kubernetes_node_pool" "node_premium" {
-  cluster_id = digitalocean_kubernetes_cluster.k8s_iniciativa_DevOps.id
+  cluster_id = digitalocean_kubernetes_cluster.k8s_iniciativa.id
 
   name       = "premium"
   size       = "s-4vcpu-8gb"
   node_count = 2
 }
 
+
 variable "do_token" {}
-
 variable "k8s_name" {}
-
 variable "region" {}
+
+output "kube_endpoint" {
+    value = digitalocean_kubernetes_cluster.k8s_iniciativa.endpoint
+  
+}
+
+#Pegando o kubeconfig
+#Obs. necessário o init(baixar plugin)
+resource "local_file" "kube_config" {
+     content  = digitalocean_kubernetes_cluster.k8s_iniciativa.kube_config.0.raw_config
+     filename = "kube_config.yaml"
+ }
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+
+do_token = "dop_v1_f0520b2247d9389d24247cf251f16779b8df31edaca757f387c4835c1546bf57"
+k8s_name = "k8s-iniciativa-devops"
+region = "nyc1"
